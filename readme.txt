453cb7b884f7d2ef4a8f108ea9f4300622a7e25b
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2020-03-16 00:08:02 -0500

    scraper done

---
+++ readme.txt
@@ -0,0 +1,3 @@
+Install - https://selenium-python.readthedocs.io/installation.html#introduction
+    pip install selenium
+    need to grab the driver for your browser
---
+++ scraper.py
@@ -0,0 +1,55 @@
+#scrapes your past youtube history 
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+if __name__ == "__main__":
+
+    #initialize everything
+    driver = webdriver.Chrome();
+    driver.get("https://www.youtube.com/feed/history")
+    input("Please log in and press any key once complete.\n")
+    # driver.get("https://www.reddit.com")
+
+    lastScroll = None #how far have we scrolled
+    lastScroll = driver.execute_script("return document.body.children[2].scrollHeight")
+
+    stuckInstances = 0
+
+    #do the scraping operation
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.children[2].scrollHeight);")
+
+        #TODO - some form of exponential backoff
+        time.sleep(1)
+
+        nowheight = driver.execute_script("return document.body.children[2].scrollHeight")
+        if(nowheight == lastScroll):
+            stuckInstances+=1
+            print("Stuck counter: " + str(stuckInstances))
+            if(stuckInstances > 10):
+                input("About to stop scraping after next enter.")
+                break
+        else:
+            stuckInstances = 0
+            lastScroll = nowheight
+
+    print("Finished scraping, logging to disk.")
+
+    #create a timestamp
+    nowTime = time.time()
+
+    #select just the one element that contains all the videos watched
+    selectedData = driver.find_element_by_id('contents')
+
+    #dump the scraped data to the disk:
+    with open("rawOutput."+str(nowTime)+".html", 'w', encoding='utf8') as f:
+        f.write(driver.page_source)
+
+    with open("selectedOutput."+str(nowTime)+".html", 'w',  encoding='utf8') as f:
+        f.write(selectedData.get_attribute('outerHTML'))
+
+    input("The scraping is concluded. Press any key to continue.")
+    driver.close()
