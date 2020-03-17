c2efb050ddd659ee32fddea5b49a95f27d95cd64
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2020-03-16 22:25:36 -0500

    new scraper - done?

--- parser.py
+++ parser.py
@@ -2,6 +2,8 @@
 
 from os import listdir
 from os.path import isfile
+import time
+from html.parser import HTMLParser
 
 #return the last file from selected output
 #that was in the current working directory
@@ -9,18 +11,42 @@ from os.path import isfile
 #   none on error
 def getLastSelectedOutput():
     toReturn = None
-    lastDate = float("inf")
+    lastDate = 0
 
     t = listdir() #get all files in directory
     for e in t:
-        if(e[0] != 's')
+        if e.find("selectedOutput") == -1:
             continue
-        if e.find("selectedOutput") == -1"
-            continue
-    
+        
+        #so this is a valid file, but is it the most recent?
+        #get the timestamp
+        n = float(e[15:len(e)-5])
+        if(n > lastDate):
+            lastDate = n
+            toReturn = e
+
     return toReturn
+
+
+
+#inPath - path to the file containing parse ready data
+# outPath - path to the output file
+#output is formatted as follows:
+#   one video per line: "<youtube id> <video length>"
+def parseVideoData(inPath, outPath):
+    ctr = 0
+
+
+    start_time = time.time()
+    with open(inPath, 'r', encoding='utf8') as inFile:
+
+    print(ctr)
+    print("Simple Parsing concluded in " + str(round(time.time()-start_time, 2)) + " seconds.")
+    
 
 if __name__ == "__main__":
     
 
     s = getLastSelectedOutput()
+    print("Loading file: " + str(s))
+    parseVideoData(s, 'parsedOutput')
--- scraper.py
+++ scraper.py
@@ -1,55 +1,57 @@
-#scrapes your past youtube history 
+#scrapes your past youtube history
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
 #TODO - replace most strings with formatted strings
+#TODO - add a timer to track how long this takes
 
 if __name__ == "__main__":
 
     #initialize everything
-    driver = webdriver.Chrome();
+    driver = webdriver.Chrome()
     driver.get("https://www.youtube.com/feed/history")
     input("Please log in and press any key once complete.\n")
 
-    #NOTE - normally use document.body.scrollHeight to get doc height
-    #   but for youtube, the HTML is configured differently and needs this
-    lastScroll = driver.execute_script("return document.body.children[2].scrollHeight")
+    start_time = time.time()
 
-    stuckInstances = 0
+    with open("selectedOutput."+str(start_time)+".html", 'w',  encoding='utf8') as f:
 
-    #do the scraping operation
-    while True:
-        driver.execute_script("window.scrollTo(0, document.body.children[2].scrollHeight);")
+        #NOTE - normally use document.body.scrollHeight to get doc height
+        #   but for youtube, the HTML is configured differently and needs this
+        lastScroll = driver.execute_script(
+            "return document.body.children[2].scrollHeight")
 
-        #TODO - some form of exponential backoff
-        time.sleep(1)
+        stuckInstances = 0
 
-        nowheight = driver.execute_script("return document.body.children[2].scrollHeight")
-        if(nowheight == lastScroll):
-            stuckInstances+=1
-            print("Stuck counter: " + str(stuckInstances))
-            if(stuckInstances > 10):
-                break
-        else:
-            stuckInstances = 0
-            lastScroll = nowheight
+        #do the scraping operation
+        while True:
+            driver.execute_script("window.scrollTo(0, document.body.children[2].scrollHeight);")
 
-    print("Finished scraping, logging to disk.")
+            #TODO - some form of exponential backoff
+            time.sleep(5)
 
-    #create a timestamp
-    nowTime = time.time()
+            nowheight = driver.execute_script("return document.body.children[2].scrollHeight")
 
-    #select just the one element that contains all the videos watched
-    selectedData = driver.find_element_by_id('contents')
+            if(nowheight == lastScroll):
+                stuckInstances += 1
+                print("Stuck counter: " + str(stuckInstances))
+                if(stuckInstances > 5):
+                    break
+            else:
+                #new data was loaded
 
-    #dump the scraped data to the disk:
-    with open("rawOutput."+str(nowTime)+".html", 'w', encoding='utf8') as f:
-        f.write(driver.page_source)
+                #find the relevant parts
+                selectedData = driver.find_element_by_id('contents')
+                f.write(selectedData.get_attribute('innerHTML'))
+                #remove the part we just logged
+                driver.execute_script("document.getElementById('contents').innerHTML = \"\"")
 
-    with open("selectedOutput."+str(nowTime)+".html", 'w',  encoding='utf8') as f:
-        f.write(selectedData.get_attribute('outerHTML'))
 
-    input("The scraping is concluded. Press any key to continue.")
+                stuckInstances = 0
+                lastScroll = nowheight
+
+    print("Scraping concluded successfully in " +
+          str(time.time()-start_time-25)+" seconds.")
     driver.close()
